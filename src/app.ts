2c34b7981d040240fbf8be35db04547bf463d1aa
AleLariccia <alejandrolariccia92@gmail.com>
2021-04-24 18:49:04 -0300

    add projectList class

--- src/app.ts
+++ src/app.ts
@@ -38,12 +38,12 @@ const validate = (validatableInput: Validatable) => {
   ) {
     isValid = isValid && validatableInput.value >= validatableInput.min;
   }
-  
+
   if (
     validatableInput.max !== null &&
     validatableInput.max !== undefined &&
     typeof validatableInput.value === "number"
-    ) {
+  ) {
     isValid = isValid && validatableInput.value <= validatableInput.max;
   }
 
@@ -59,6 +59,41 @@ const Autobind = (_: any, _2: string, descriptor: PropertyDescriptor) => {
     },
   };
 };
+
+class ProjectList {
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  element: HTMLElement;
+
+  constructor(private type: "active" | "finished") {
+    this.templateElement = document.getElementById(
+      "project-list"
+    )! as HTMLTemplateElement;
+
+    this.hostElement = document.getElementById("app")! as HTMLDivElement;
+
+    const importedNode = document.importNode(
+      this.templateElement.content,
+      true
+    );
+    this.element = importedNode.firstElementChild as HTMLElement;
+    this.element.id = `${this.type}-projects`;
+
+    this.attach();
+    this.renderContent();
+  }
+
+  private attach() {
+    this.hostElement.insertAdjacentElement("beforeend", this.element);
+  }
+
+  private renderContent() {
+    const listId = `${this.type}-projects-list`;
+    this.element.querySelector("ul")!.id = listId;
+    this.element.querySelector("h2")!.textContent =
+      this.type.toUpperCase() + " PROJECTS";
+  }
+}
 
 class ProjectInput {
   templateElement: HTMLTemplateElement;
@@ -148,3 +183,5 @@ class ProjectInput {
 }
 
 const projectInput = new ProjectInput();
+const activeProjectList = new ProjectList("active");
+const finishedProjectList = new ProjectList("finished");
