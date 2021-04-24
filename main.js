12ec4a62c00eff9c4513f534652c6c26419ce5a4
AleLariccia <alejandrolariccia92@gmail.com>
2021-04-23 22:36:58 -0300

    DOM Element Selection & OOP Rendering

--- main.js
+++
@@ -1,9 +0,0 @@
-var firstInput = document.getElementById("fist-value");
-var secondInput = document.getElementById("second-value");
-var submit = document.getElementById("submit");
-var add = function (numA, numB) {
-    return numA + numB;
-};
-submit.addEventListener('click', function () {
-    console.log('suma', add(firstInput.value, secondInput.value));
-});
